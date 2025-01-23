313306bb995aee557d9628dcb2511d7cc6dd423a
Rithikashreesiva <rithikashreesivasamy03@gmail.com>
2025-01-23 16:35:57 +0530

    Fix environment variable loading

--- AutoCarePro/backend/index.js
+++ AutoCarePro/backend/index.js
@@ -5,6 +5,9 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoute = require('./routes/userRoutes')
 const BookingRoute = require('./routes/bookingRoutes')
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 dotenv.config();
 connectDB();
@@ -18,3 +21,8 @@ app.use('/api/bookings', BookingRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} : http://localhost:${PORT}`));
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('MongoDB URI:', process.env.MONGODB_URI);
